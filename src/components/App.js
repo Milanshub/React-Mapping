566c87bd8e94f0f63ef54c78e745bc2dae16a974
Milan <m26030820@gmail.com>
2024-01-08 20:41:30 +0100

    learning arrow function and implementing on react functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,32 @@
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEmoji(emoji){
-  return <Entry 
-  key={emoji.id}
-  emoji={emoji.emoji} 
-  name={emoji.name}
-  meaning={emoji.meaning}
-   />
-}
+// Before we uas the arrow function 
+// function createEmoji(emoji){
+//   return <Entry 
+//   key={emoji.id}
+//   emoji={emoji.emoji} 
+//   name={emoji.name}
+//   meaning={emoji.meaning}
+//    />
+// }
 
 
+//Below arrow function is used, instead of the above function. 
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">
-
-      {emojipedia.map(createEmoji)}
+      <dl className="dictionary"> {emojipedia.map(emoji =>  (
+      <Entry 
+          key={emoji.id}
+          emoji={emoji.emoji} 
+          name={emoji.name}
+          meaning={emoji.meaning}
+        />
+        ))}
 
       
       </dl>
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
 // function double(x){
 //   return x *2 
 // }  
-// const newNumer = numbers.map(double);
+// const newNumbers = numbers.map(double);
 
 
 //1.1 - Equivalent to the map function but requires more effort
@@ -25,6 +25,9 @@ root.render(
 //newNumbers.push(x * 2);  
 //}
 //number.forEach(double);
+
+//1.2 - Map - using arrow function 
+// const newNumbers = numbers.map ( x => x * 2); 
 
 //2.0 - Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(fucntion(num){
@@ -39,6 +42,11 @@ root.render(
 //}
 //})
 
+//2.2 - Filter - using arrow function 
+// const newNumbers = numbers.filter( num =>
+//    num > 10
+// );
+
 //3.0 - Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(accumulator, currentNumber){
 //return accumulator + newNumber  
@@ -50,16 +58,30 @@ root.render(
 // mewNMumber += currentNumber 
 //});
 
+// 3.2 - Reduce - using arrow function 
+// var newNumber = numbers.reduce(accumulator, currentNumber) => 
+// accumulator + newNumber  
+//);
+
 //4.0 - Find - find the first item that matches from an array. Not loop through entire array but stop when finds. 
 // const newNumber=numbers.find(function(num){
 // return num > 10; 
 //}); 
 
+//4.1 - Find - using arrow function 
+// const newNumber=numbers.find( num =>
+//  num > 10; 
+//); 
 
 //5.0 - FindIndex - find the index of the first item that matches.
 // const newNumber=numbers.find(function(num){
 // return num > 10; 
 //}); 
+
+//5.1 Findindex - using arrow function 
+// const newNumber=numbers.find( num => 
+// num > 10; 
+//); 
 
 // return a max of 100 characted "meaning" object from emojipedia.js 
 const newEmojipedia = emojipedia.map(function (emojiEntry){
@@ -67,3 +89,17 @@ const newEmojipedia = emojipedia.map(function (emojiEntry){
 }); 
 
 console.log(newEmojipedia); 
+
+//6.0 Arrow function. Replaces the world function or its name by an arrow. "( )" not needed
+// if there is 1 input, only when there is more than 1 inputs. Can also delete "return" and
+// curly braces. 
+// const newNumbers = numbers.map( x => 
+//   x * x
+// );
+
+// 6.1 If we are not to use the arrrow function 
+// const newNumbers = numbers.map(function (x){
+//   return x * x;
+// });
+
+// console.log(newNumbers);
